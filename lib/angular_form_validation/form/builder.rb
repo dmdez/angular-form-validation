0967040d3e90bbbf08934c8ab5b81939199429df
Deric Mendez <deric@cremalab.com>
2015-07-28 15:03:37 -0500

    fix escape, undefined method

--- lib/angular_form_validation/form/builder.rb
+++ lib/angular_form_validation/form/builder.rb
@@ -9,7 +9,7 @@ module AngularFormValidation
 
     FIELD_HELPERS.each do |method_name|
       define_method(method_name) do |method, options = {}|
-        options.merge!({"ng-model" => "#{@object_name}.#{method.to_s}", "ng-init" => "#{@object_name}.#{method.to_s}='#{escape_value(@object[method.to_s])}'"})
+        options.merge!({"ng-model" => "#{@object_name}.#{method.to_s}", "ng-init" => "#{@object_name}.#{method.to_s}='#{@object[method.to_s].to_s.gsub("'"){"\\'"}}'"})
         super(method, options)
       end
     end
@@ -43,12 +43,6 @@ module AngularFormValidation
       html_options.merge!({"ng-model" => "#{@object_name}.#{method.to_s}", "ng-init" => "#{@object_name}.#{method.to_s}='#{@object[method.to_s]}'"})
       super(method, collection, value_method, text_method, options = {}, html_options, &block)
     end
-
-    def escape_value(value)
-      value.to_s.gsub("'"){"\\'"}
-    end
-
-    private_class_method :escape_value
 
   end
 
